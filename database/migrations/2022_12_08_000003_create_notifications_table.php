f40355eec7cee9b1a48649d5ac8304d67ea6d723
Luca Samore <luca.samore@studio.unibo.it>
2022-12-08 18:47:17 +0100

    Create notifications migration

---
+++ database/migrations/2022_12_08_000003_create_notifications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('from');
+            $table->uuid('to');
+            $table->string('type');
+            $table->dateTime('createdAt');
+            $table->primary('id');
+            $table->foreign('from')->references('id')->on('users');
+            $table->foreign('to')->references('id')->on('users');
+            $table->foreign('type')->references('name')->on('notification_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
