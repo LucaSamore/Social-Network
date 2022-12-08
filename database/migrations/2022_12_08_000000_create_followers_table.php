b696b2382197b1e5fa5b0ae642736fd6cfd440ce
Luca Samore <luca.samore@studio.unibo.it>
2022-12-08 18:12:51 +0100

    Create followers migration

---
+++ database/migrations/2022_12_08_000000_create_followers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->uuid('follower');
+            $table->uuid('followee');
+            $table->primary(['follower','followee']);
+            $table->foreign('follower')->references('id')->on('users');
+            $table->foreign('followee')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+};
