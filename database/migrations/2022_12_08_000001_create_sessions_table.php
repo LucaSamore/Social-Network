0146ef631266fa7ce45f67f686cd8d689c5dbc9c
Luca Samore <luca.samore@studio.unibo.it>
2022-12-08 18:24:15 +0100

    Create sessions migration

---
+++ database/migrations/2022_12_08_000001_create_sessions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('userId');
+            $table->dateTime('createdAt');
+            $table->dateTime('expiresAt');
+            $table->primary('id');
+            $table->foreign('userId')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
