d3cd6b21a8cbeb5fc38ae494c09270b5d05c4e51
Luca Samore <luca.samore@studio.unibo.it>
2022-12-08 20:54:10 +0100

    Create likes migration

---
+++ database/migrations/2022_12_08_000006_create_likes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->uuid('userId');
+            $table->uuid('postId');
+            $table->primary(['userId','postId']);
+            $table->foreign('userId')->references('id')->on('users');
+            $table->foreign('postId')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+};
