8d46ef8c566d2f1811fa2e69439aecab1d3e8a7d
Luca Samore <luca.samore@studio.unibo.it>
2022-12-08 21:04:09 +0100

    Create comments migration

---
+++ database/migrations/2022_12_08_000007_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->text('textualContent');
+            $table->bigInteger('numberOfLikes');
+            $table->uuid('userId');
+            $table->uuid('postId');
+            $table->primary('id');
+            $table->foreign('userId')->references('id')->on('users');
+            $table->foreign('postId')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
