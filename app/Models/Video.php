0859102574bb0f67228298bef4804f792bab873d
Luca Samore <luca.samore@studio.unibo.it>
2022-12-09 11:27:45 +0100

    Create Image and Video models

---
+++ app/Models/Video.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    use HasFactory, HasUuids;
+
+    public $incrementing = false;
+    public $timestamps = false;
+}
