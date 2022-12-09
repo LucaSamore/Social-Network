46f785c5f97b2ff17019b9566b23a12f9d92281a
Luca Samore <luca.samore@studio.unibo.it>
2022-12-09 10:42:23 +0100

    Create Post model

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory, HasUuids;
+
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'textual_content'
+    ];
+}
