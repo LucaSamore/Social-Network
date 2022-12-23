fdc20c2fb534bc1a674c899a08996c7167583a38
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-12-23 16:33:44 +0100

    add leftMenu

--- resources/views/leftMenu.blade.php
+++ resources/views/leftMenu.blade.php
@@ -11,33 +11,42 @@
 </head>
 
 <body class="bg-white w-full h-screen">
-    <aside class="w-1/3 h-screen bg-black">
-        <ul class="menu bg-black w-56 p-2 rounded-box">
-            <li>
-                <a class="flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" class=""  fill="#000" stroke="currentColor">
+    <aside class="w-1/4 h-screen bg-black text-white">
+
+        <ul class="menu bg-black h-auto card-body justify-center">
+            <li class="w-full h-auto p-2">
+                <a class="flex w-fit h-fit rounded-full py-2 px-4 hover:bg-blue-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="basis-1/3 h-8 w-8 mx-2" fill="#000" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
-                    Item 1
+                    <strong>Home</strong>
                 </a>
             </li>
-            <li>
-                <a class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="" fill="#fff" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+
+            <li class="w-full h-auto p-2">
+                <a class="flex w-fit h-fit rounded-full py-2 px-4 hover:bg-blue-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="basis-1/3 h-8 w-8 mx-2" fill="#000" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
-                    Item 2
+                    <strong>Home</strong>
                 </a>
             </li>
-            <li>
-                <a class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="" fill="#fff" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+
+            <li class="w-full h-auto p-2 card-actions justify-center">
+                <a class="w-fit h-fit rounded-full py-2 px-4 hover:bg-blue-500 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="basis-1/3 h-8 w-8 mx-2" fill="#000" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
-                    Item 3
+                    <strong>Home</strong>
                 </a>
             </li>
+
         </ul>
+
+        <div class="card-actions justify-center">
+            <button class="btn btn-primary rounded-full">NEW POST</button>
+        </div>
+
     </aside>
 </body>
 
