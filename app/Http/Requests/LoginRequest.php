3345fda4a0f1c1282eaab6de67a7540fdf2184ed
LucaSamore <luca.samore.01@gmail.com>
2022-12-24 19:12:35 +0100

    Better validation rules and custom error messages for requests

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -7,6 +7,21 @@ use Illuminate\Foundation\Http\FormRequest;
 final class LoginRequest extends FormRequest
 {
     /**
+     * Indicates if the validator should stop on the first rule failure.
+     *
+     * @var bool
+     */
+    protected $stopOnFirstFailure = true;
+
+    /**
+     * The route that users should be redirected to if validation fails.
+     *
+     * @var string
+     */
+    protected $redirectRoute = 'login';
+
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, mixed>
@@ -14,8 +29,23 @@ final class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => ['required', 'email', 'unique:users,email'],
-            'password' => ['required'],
+            'email' => ['required', 'email', 'string'],
+            'password' => ['required', 'string'],
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'email.required' => 'Questo campo &egrave; obbligatorio',
+            'password.required' => 'Questo campo &egrave; obbligatorio',
+            'email.email' => 'Il contenuto non &egrave; una email valida'
+        ];
+    }
+
 }
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,9 +3,25 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 final class RegisterRequest extends FormRequest
 {
+    /**
+     * Indicates if the validator should stop on the first rule failure.
+     *
+     * @var bool
+     */
+    protected $stopOnFirstFailure = true;
+
+    /**
+     * The route that users should be redirected to if validation fails.
+     *
+     * @var string
+     */
+    protected $redirectRoute = 'register';
+
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -18,11 +34,32 @@ final class RegisterRequest extends FormRequest
             'surname' => ['required', 'string'],
             'username' => ['required', 'string', 'unique:users,username'],
             'email' => ['required', 'email', 'unique:users,email'],
-            'password' => ['required'],
+            'password' => ['required', Password::min(8)->letters()->numbers()->mixedCase()->symbols()],
             'date_of_birth' => ['required', 'date'],
             'bio' => ['string', 'nullable'],
             'profile_image' => ['string', 'nullable'],
             'banner_image' => ['string', 'nullable'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Questo campo &egrave; obbligatorio',
+            'surname.required' => 'Questo campo &egrave; obbligatorio',
+            'username.required' => 'Questo campo &egrave; obbligatorio',
+            'username.unique:users,username' => 'Username gi&agrave; in uso',
+            'email.required' => 'Questo campo &egrave; obbligatorio',
+            'email.unique:users,email' => 'Email gi&agrave; in uso',
+            'password.required' => 'Questo campo &egrave; obbligatorio',
+            'email.email' => 'Il contenuto non &egrave; una email valida',
+            'date_of_birth.required' => 'Questo campo &egrave; obbligatorio',
+            'date_of_birth.date' => 'La data inserita non &egrave; valida',
         ];
     }
 }
