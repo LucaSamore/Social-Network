972d0750ff0722c2598c10dc0d120f7c78c89b7b
LucaSamore <luca.samore.01@gmail.com>
2022-12-29 22:47:42 +0100

    Add side menu component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         @vite('resources/css/app.css')
     </head>
-    <body>
-        <h1 class="text-3xl font-bold underline">Home</h1>
-        <a href="/logout">Logout</a>
+    <body class="flex">
+        <x-sidemenu/>
+
+        <main class="w-3/5">
+            <header>
+                <h1>Feed</h1>
+            </header>
+            <section>
+
+            </section>
+        </main>
+        <aside class="w-1/5">
+            <input type="search" name="search" id="search"/>
+            <section class="flex flex-col bg-dark-mode-2 rounded-xl">
+                <h2>I trend più seguiti</h2>
+                <section class="flex gap-6">
+                    <ul>
+                        <li>#Mondiali2022</li>
+                        <li>#F1</li>
+                        <li>#GoldenRetriever</li>
+                        <li>#SanRemo2023</li>
+                        <li>#UniBO</li>
+                    </ul>
+                    <ul>
+                        <li>4.5M</li>
+                        <li>3.4M</li>
+                        <li>1.2M</li>
+                        <li>670k</li>
+                        <li>56k</li>
+                    </ul>
+                </section>
+            </section>
+        </aside>
     </body>
+    <script src="https://kit.fontawesome.com/6b12fba364.js" crossorigin="anonymous"></script>
 </html>
