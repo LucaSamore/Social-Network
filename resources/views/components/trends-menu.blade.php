2cdf71f12625d3f832a13cd6ef188410690a427f
LucaSamore <luca.samore.01@gmail.com>
2022-12-29 23:48:22 +0100

    Add top trends component

---
+++ resources/views/components/trends-menu.blade.php
@@ -0,0 +1,23 @@
+<aside class="w-1/4 xl:1/5 lg:1/4 md:1/4 sm:1/4 hidden xl:flex lg:hidden md:hidden sm:hidden flex-col justify-center items-center gap-12">
+    <section class="flex flex-col justify-start items-center gap-6 bg-dark-mode-2 rounded-xl w-4/5 h-1/3 overflow-auto">
+        <header class="text-center">
+            <h2 class="text-white font-montserrat text-lg xl:text-xl lg:text:lg md:text-sm sm:text-sm mt-8">I Trend più seguiti</h2>
+        </header>
+        <section class="flex gap-12 text-white font-quicksand py-4">
+            <ul class="font-bold flex flex-col gap-4">
+                <li>#Mondiali2022</li>
+                <li>#F1</li>
+                <li>#GoldenRetriever</li>
+                <li>#SanRemo2023</li>
+                <li>#UniBO</li>
+            </ul>
+            <ul class="flex flex-col gap-4">
+                <li>4.5M</li>
+                <li>3.4M</li>
+                <li>1.2M</li>
+                <li>670k</li>
+                <li>56k</li>
+            </ul>
+        </section>
+    </section>
+</aside>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,7 @@
         @vite('resources/css/app.css')
     </head>
     <body class="flex">
-        <x-sidemenu/>
-
+        <x-side-menu/>
         <main class="w-3/5">
             <header>
                 <h1>Feed</h1>
@@ -19,28 +18,7 @@
 
             </section>
         </main>
-        <aside class="w-1/5">
-            <input type="search" name="search" id="search"/>
-            <section class="flex flex-col bg-dark-mode-2 rounded-xl">
-                <h2>I trend più seguiti</h2>
-                <section class="flex gap-6">
-                    <ul>
-                        <li>#Mondiali2022</li>
-                        <li>#F1</li>
-                        <li>#GoldenRetriever</li>
-                        <li>#SanRemo2023</li>
-                        <li>#UniBO</li>
-                    </ul>
-                    <ul>
-                        <li>4.5M</li>
-                        <li>3.4M</li>
-                        <li>1.2M</li>
-                        <li>670k</li>
-                        <li>56k</li>
-                    </ul>
-                </section>
-            </section>
-        </aside>
+        <x-trends-menu/>
     </body>
     <script src="https://kit.fontawesome.com/6b12fba364.js" crossorigin="anonymous"></script>
 </html>
