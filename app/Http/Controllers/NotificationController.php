bad5174526c48a3a194dc945417ff278a9f9c1ae
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2023-01-05 17:00:03 +0100

    modift show method

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Follower;
 use App\Models\Notification;
 use App\Models\NotificationType;
 use App\Models\User;
@@ -17,9 +18,27 @@ class NotificationController extends Controller
      */
     public function show(User $userId, int $n)
     {
-        $data = Notification::where('to', $userId->id)->orderBy('created_at', 'desc')->take($n)->get()[0];
-        echo $data;
-        return View('Notification', $data);
+        //$userId = $request->session()->get("user_id");
+        $notifications = Notification::where('to', $userId->id)->orderBy('created_at', 'desc')->take($n)->get();
+        
+        $today = date_create(date('Y-m-d H:i:s'));
+        foreach($notifications as $notify){
+            $notifyDate = date_create($notify->created_at);
+            //echo "today: ".$today->format('Y-m-d H:i:s')."<br>";
+            //echo "notify: ".$notifyDate->format('Y-m-d H:i:s')."<br>";
+            $diff = $today->diff($notifyDate);
+            $notify->created_at = $diff->format('%a d %h h %i m %s s');
+            $notify->fromUsername = User::findOrFail($notify->from)->username; 
+            //echo $notify->from."<br>";
+            //echo $userId->id."<br>";
+            $notify->follow = Follower::where('follower',  $notify->from)->where('followee', $userId->id)->exists();
+            //var_dump($notify->follow);
+        }
+
+        //echo $userId;
+        //$username = User::findOrFail($userId);
+        //echo $userId->username;
+        return View('Notification', ['notifications' => $notifications]);
     }
 
   
@@ -45,6 +64,7 @@ class NotificationController extends Controller
     }
 
     public function notifyCommentToPost(User $userDo, User $userReceive){
+
         NotificationController::store($userDo, $userReceive, NotificationType::findOrFail("ha commentato un tuo post"));  
         return back();
     }
