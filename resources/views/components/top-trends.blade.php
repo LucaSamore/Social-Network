d1ea0db1a47a5b6d239971843399dfc6bd466689
LucaSamore <luca.samore.01@gmail.com>
2023-01-13 10:36:02 +0100

    Better top trends

--- resources/views/components/top-trends.blade.php
+++ resources/views/components/top-trends.blade.php
@@ -1,16 +1,16 @@
 <aside class="w-1/6 xl:1/5 lg:1/5 md:1/6 sm:1/6 hidden px-auto xl:flex lg:hidden md:hidden sm:hidden flex-col justify-center items-center gap-12">
     <x-search-bar/>
-    <section class="flex flex-col justify-start items-center gap-6 bg-dark-mode-2 rounded-lg w-4/5 h-1/3 overflow-auto px-6">
+    <section class="flex flex-col justify-evenly items-center gap-6 bg-dark-mode-2 rounded-lg w-4/5 h-2/5 overflow-auto px-6">
         <header class="text-center">
-            <h2 class="text-white font-quicksand font-bold text-lg xl:text-lg lg:text:lg md:text-sm sm:text-sm mt-8">Trend più seguiti</h2>
+            <h2 class="text-white font-montserrat font-bold text-lg xl:text-lg lg:text:lg md:text-sm sm:text-sm">Trend più seguiti 🤩</h2>
         </header>
-        <div class="flex gap-12 text-white font-quicksand py-4">
-            <ul class="font-bold flex flex-col gap-4">
+        <div class="flex gap-12 text-white font-quicksand">
+            <ul class="font-bold flex flex-col gap-4 text-lg">
                 @foreach ($trends as $trend)
                     <li>{{ $trend->name }}</li>
                 @endforeach
             </ul>
-            <ul class="flex flex-col gap-4">
+            <ul class="flex flex-col gap-4 text-lg">
                 @foreach ($trends as $trend)
                     <li>{{ $trend->posts_count }}</li>
                 @endforeach
